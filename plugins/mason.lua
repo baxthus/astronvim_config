2e90ed414683f441821d777da20f5a4233695514
baxthus <baxthus@proton.me>
2024-01-07 18:31:08 -0300

    Remove prettier and eslint-lsp

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -23,8 +23,6 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
         -- "stylua",
-        "prettier",
-        "eslint-lsp",
       })
     end,
   },
